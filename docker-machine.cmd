24f4c4f5aa48dd8647e9f9e35a6888c4570951fb
Thach Nguyen <thachnn80@gmail.com>
2022-08-05 14:51:41 +0700

    Update `docker-machine` script to allow VM snapshot reverting

--- docker-machine.cmd
+++ docker-machine.cmd
@@ -1,5 +1,5 @@
 @ECHO OFF
-SetLocal
+SETLOCAL
 
 IF EXIST "%DOCKER_MACHINE_VMX%" GOTO parse_arguments
 
@@ -8,17 +8,24 @@ ECHO SET DOCKER_MACHINE_VMX=path/to/docker-machine.vmx
 EXIT /B 1
 
 :parse_arguments
+SET "_CMD=%~1"
 SET "EXTRA_ARGS=%~2"
-IF "%~1"=="start" GOTO process_arguments
-IF "%~1"=="stop" GOTO start_process
-IF "%~1"=="reset" GOTO start_process
-IF "%~1"=="suspend" GOTO start_process
+IF "%_CMD%"=="start" GOTO process_start_cmd
+IF "%_CMD%"=="stop" GOTO start_process
+IF "%_CMD%"=="reset" GOTO start_process
+IF "%_CMD%"=="suspend" GOTO start_process
+IF "%_CMD%"=="revert" GOTO process_revert_cmd
 
-ECHO Usage: %~n0 [start^|stop^|reset^|suspend]
+ECHO Usage: %~n0 [start^|stop^|reset^|suspend^|revert]
 EXIT /B 1
 
-:process_arguments
+:process_start_cmd
 IF "%EXTRA_ARGS%"=="" SET EXTRA_ARGS=nogui
+GOTO start_process
+
+:process_revert_cmd
+SET "_CMD=%_CMD%ToSnapshot"
+IF "%EXTRA_ARGS%"=="" SET EXTRA_ARGS=Initialized
 
 :start_process
 SET _APP_PATH=
@@ -26,4 +33,4 @@ FOR /F "skip=2 tokens=2*" %%H IN (
     'REG QUERY "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\vmplayer.exe" /ve'
 ) DO SET "_APP_PATH=%%~dpI"
 
-"%_APP_PATH%vmrun" %1 "%DOCKER_MACHINE_VMX%" %EXTRA_ARGS%
+"%_APP_PATH%vmrun" %_CMD% "%DOCKER_MACHINE_VMX%" %EXTRA_ARGS%
